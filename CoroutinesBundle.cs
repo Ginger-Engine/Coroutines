db2b83e7e86f0be14b732ad6f5ef68483f0587e2
aleksey.pilov <aleksey.pilov@yandex.ru>
2025-07-06 20:34:58 +0300

    clean

---
+++ CoroutinesBundle.cs
@@ -0,0 +1,12 @@
+﻿using Engine.Core;
+using GignerEngine.DiContainer;
+
+namespace Engine.Coroutines;
+
+public class CoroutinesBundle : IBundle
+{
+    public void InstallBindings(DiBuilder builder)
+    {
+        builder.Bind<CoroutinesManager>();
+    }
+}
